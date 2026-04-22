56d8cf0efb627d7ad3bf5db65a2355855a6522ec
katm6211 <kmalenovic07@gmail.com>
2026-04-21 23:15:42 -0700

    flame changes

--- docs/src/scenes/slide1.js
+++ docs/src/scenes/slide1.js
@@ -22,18 +22,26 @@ class Slide1 extends Phaser.Scene
             const border = this.add.image(width/4, height/2, 'border');
             border.setDisplaySize(width/2, height); 
             const arthur = this.add.image(width/4, height/2, 'arthur');
-            arthur.setScale(0.45); 
-
-            this.time.delayedCall(10000, () => {
+            arthur.setScale(0.35); 
+            this.time.delayedCall(5000, () => {
                 const dragon = this.add.image(800, 0, 'dragon');
                 this.tweens.add({
                     targets: dragon,
                     x: 600,
                     y: 100,
-                    alpha: 0,
-                    duration: 10000,
+                    alpha: 1,
+                    duration: 3000,
                     ease: 'Linear',
-                    repeat: 0 
+                    repeat: 0,
+                    onComplete: () => {
+                    const flame = this.add.image(width*5/8, height*5/8).setAlpha(0);
+                        this.tweens.add({
+                            targets: flame,
+                            alpha: 1,
+                            duarion: 3000,
+                            ease: 'Power2'
+                        });
+                    }
                 });
             }, [], this);
 
