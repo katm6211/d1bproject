2244a10ccb975c1a9e5d493deb77c6fccb04bb99
katm6211 <kmalenovic07@gmail.com>
2026-04-22 20:07:17 -0700

    revision slide 4

--- docs/src/scenes/slide4.js
+++ docs/src/scenes/slide4.js
@@ -4,7 +4,7 @@ class Slide4 extends Phaser.Scene {
     }
     preload() {
         this.load.setBaseURL('https://katm6211.github.io/d1bproject/');
-        this.load.image('bg', 'assets/slide4/background.png')
+        this.load.image('bg1', 'assets/slide4/background.png')
         this.load.image('banner', 'assets/slide4/banner.png');
         this.load.image('continue1', 'assets/slide4/continue.png');
         this.load.image('credits', 'assets/slide4/credits.png');
@@ -15,7 +15,7 @@ class Slide4 extends Phaser.Scene {
 
     create() {
         const { width, height } = this.scale;
-        const background = this.add.image(width / 2, height / 2, 'background').setAlpha(0);
+        const bg1 = this.add.image(width / 2, height / 2, 'background').setAlpha(0);
         const banner = this.add.image(420, 200, 'banner').setAlpha(0).setScale(0.7);
         const continue1 = this.add.image(width * 3 / 8, height * 3.8 / 8, 'continue1').setAlpha(0).setScale(0.7);
         const credits = this.add.image(width * 4.25 / 8, height * 4.5 / 8, 'credits').setAlpha(0).setScale(0.7);
@@ -67,11 +67,11 @@ class Slide4 extends Phaser.Scene {
 
 
 
-        let bg = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'bg');
+        let bg = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'bg1');
 
 
-        let scaleX = this.cameras.main.width / bg.width;
-        let scaleY = this.cameras.main.height / bg.height;
+        let scaleX = this.cameras.main.width / bg1.width;
+        let scaleY = this.cameras.main.height / bg1.height;
 
 
         let scale = Math.max(scaleX, scaleY);
